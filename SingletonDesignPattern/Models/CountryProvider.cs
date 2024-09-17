0266451d364110768815d2dc4b041e6c8dda7cbd
Ilham Binyetov <ilhamgb@code.edu.az>
2024-09-17 14:57:07 +0400

    Implement Singleton Design Pattern

---
+++ SingletonDesignPattern/Models/CountryProvider.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonDesignPattern.Models
+{
+    public class CountryProvider
+    {
+        private static CountryProvider instance = null;
+
+        public static CountryProvider Instance
+        {
+            get
+            {
+               instance = instance ?? (instance = new CountryProvider());
+               return instance;
+            }
+        }
+
+        private new List<Country> Countries {  get; set; }
+
+        private CountryProvider()
+        {
+                Task.Delay(2000).GetAwaiter().GetResult();
+
+            Countries =  new List<Country>()
+            {
+                new Country(){Name = "Azerbaijan"},
+                new Country(){Name = "Turkiye"}
+            };
+
+        }
+
+
+        public async Task<List<Country>> GetCountries() => Countries;
+    }
+}
---
+++ SingletonDesignPattern/Program.cs
@@ -0,0 +1,26 @@
+﻿
+#region Explanation
+//Plus//
+// - Ensure single instance
+// - Globally access
+// - Created only when requested
+
+
+//Minus//
+// - Difficult to UnitTest
+// - In multithread word, threads cannot crate its own instance
+
+#endregion
+
+
+using SingletonDesignPattern.Models;
+Console.WriteLine(DateTime.Now.ToLongTimeString());
+var countries = await CountryProvider.Instance.GetCountries();
+
+foreach (var country in countries)
+{
+    Console.WriteLine(country.Name);
+}
+
+var countries1 =  await CountryProvider.Instance.GetCountries();
+Console.WriteLine(DateTime.Now.ToLongTimeString());
